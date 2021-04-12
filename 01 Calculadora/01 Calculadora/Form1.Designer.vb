d970051b6080a522c563e03b9663ac242048e049
Ibai Clase 1 <ibai3553@gmail.com>
2021-04-12 10:48:05 +0200

    Calculadora Nicholas Cage

---
+++ 01 Calculadora/01 Calculadora/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rbDiv = New System.Windows.Forms.RadioButton()
+        Me.rbResta = New System.Windows.Forms.RadioButton()
+        Me.rbSuma = New System.Windows.Forms.RadioButton()
+        Me.rbMulti = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtRes = New System.Windows.Forms.TextBox()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(2, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(102, 27)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Numero 1"
+        '
+        'txtNum1
+        '
+        Me.txtNum1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtNum1.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.txtNum1.Location = New System.Drawing.Point(110, 9)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(148, 34)
+        Me.txtNum1.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(2, 52)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 27)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Numero 2"
+        '
+        'txtNum2
+        '
+        Me.txtNum2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtNum2.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.txtNum2.Location = New System.Drawing.Point(110, 49)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(148, 34)
+        Me.txtNum2.TabIndex = 3
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.GroupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GroupBox1.Controls.Add(Me.rbDiv)
+        Me.GroupBox1.Controls.Add(Me.rbResta)
+        Me.GroupBox1.Controls.Add(Me.rbSuma)
+        Me.GroupBox1.Controls.Add(Me.rbMulti)
+        Me.GroupBox1.Font = New System.Drawing.Font("Comic Sans MS", 12.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.GroupBox1.Location = New System.Drawing.Point(264, -2)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(127, 130)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Opciones"
+        '
+        'rbDiv
+        '
+        Me.rbDiv.AutoSize = True
+        Me.rbDiv.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.rbDiv.Location = New System.Drawing.Point(8, 98)
+        Me.rbDiv.Name = "rbDiv"
+        Me.rbDiv.Size = New System.Drawing.Size(74, 23)
+        Me.rbDiv.TabIndex = 3
+        Me.rbDiv.Text = "Division"
+        Me.rbDiv.UseVisualStyleBackColor = True
+        '
+        'rbResta
+        '
+        Me.rbResta.AutoSize = True
+        Me.rbResta.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.rbResta.Location = New System.Drawing.Point(8, 48)
+        Me.rbResta.Name = "rbResta"
+        Me.rbResta.Size = New System.Drawing.Size(61, 23)
+        Me.rbResta.TabIndex = 1
+        Me.rbResta.Text = "Resta"
+        Me.rbResta.UseVisualStyleBackColor = True
+        '
+        'rbSuma
+        '
+        Me.rbSuma.AutoSize = True
+        Me.rbSuma.Checked = True
+        Me.rbSuma.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.rbSuma.Location = New System.Drawing.Point(8, 23)
+        Me.rbSuma.Name = "rbSuma"
+        Me.rbSuma.Size = New System.Drawing.Size(60, 23)
+        Me.rbSuma.TabIndex = 0
+        Me.rbSuma.TabStop = True
+        Me.rbSuma.Text = "Suma"
+        Me.rbSuma.UseVisualStyleBackColor = True
+        '
+        'rbMulti
+        '
+        Me.rbMulti.AutoSize = True
+        Me.rbMulti.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.rbMulti.Location = New System.Drawing.Point(8, 73)
+        Me.rbMulti.Name = "rbMulti"
+        Me.rbMulti.Size = New System.Drawing.Size(113, 23)
+        Me.rbMulti.TabIndex = 2
+        Me.rbMulti.Text = "Multiplicacion"
+        Me.rbMulti.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.Label4.Location = New System.Drawing.Point(2, 189)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(100, 27)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Resultado"
+        '
+        'txtRes
+        '
+        Me.txtRes.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.txtRes.Font = New System.Drawing.Font("Comic Sans MS", 14.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.txtRes.Location = New System.Drawing.Point(110, 186)
+        Me.txtRes.Name = "txtRes"
+        Me.txtRes.ReadOnly = True
+        Me.txtRes.Size = New System.Drawing.Size(148, 34)
+        Me.txtRes.TabIndex = 5
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnCalcular.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.btnCalcular.Location = New System.Drawing.Point(264, 134)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(127, 27)
+        Me.btnCalcular.TabIndex = 7
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = False
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnLimpiar.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.btnLimpiar.Location = New System.Drawing.Point(264, 163)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(127, 27)
+        Me.btnLimpiar.TabIndex = 8
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = False
+        '
+        'btnSalir
+        '
+        Me.btnSalir.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnSalir.Font = New System.Drawing.Font("Comic Sans MS", 9.75!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point)
+        Me.btnSalir.Location = New System.Drawing.Point(264, 193)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(127, 27)
+        Me.btnSalir.TabIndex = 9
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(398, 230)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnLimpiar)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.txtRes)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.Label1)
+        Me.Cursor = System.Windows.Forms.Cursors.Arrow
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "01 Calculadora"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rbDiv As RadioButton
+    Friend WithEvents rbMulti As RadioButton
+    Friend WithEvents rbResta As RadioButton
+    Friend WithEvents rbSuma As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtRes As TextBox
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents btnSalir As Button
+End Class
---
+++ 01 Calculadora/01 Calculadora/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtNum2.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        If Not IsNumeric(txtNum1.Text) Then
+            MessageBox.Show("Nick no esta feliz. Eso no es un numero", "Dios no existe")
+            txtNum1.Focus()
+            txtNum1.SelectAll()
+            Return
+
+        End If
+
+        If Not IsNumeric(txtNum2.Text) Then
+            MessageBox.Show("Nick no esta feliz. Eso no es un numero", "Dios no existe")
+            txtNum2.Focus()
+            txtNum2.SelectAll()
+
+            Return
+        End If
+        Dim num1 As Integer = txtNum1.Text
+        Dim num2 As Integer = txtNum2.Text
+        Dim res As Double = 0
+
+        If rbSuma.Checked Then
+            res = num1 + num2
+
+            txtRes.Text() = res
+        End If
+        If rbResta.Checked Then
+            res = num1 - num2
+
+            txtRes.Text() = res
+        End If
+        If rbMulti.Checked Then
+            res = num1 * num2
+
+            txtRes.Text() = res
+        End If
+        If rbDiv.Checked Then
+            If num2 = 0 Then
+                MessageBox.Show("Eres Debil, tus trucos no funcionan", "Dios no existe")
+                Return
+            End If
+            res = num1 / num2
+
+            txtRes.Text() = res
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        MessageBox.Show("Nunca limpiaras tus pecados", "Dios no existe")
+
+        txtNum1.Clear()
+        txtNum2.Clear()
+        txtRes.Clear()
+
+    End Sub
+
+    Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles btnSalir.Click
+        MessageBox.Show("No puedes escapar para siempre", "Dios no existe")
+
+        Close()
+
+    End Sub
+End Class

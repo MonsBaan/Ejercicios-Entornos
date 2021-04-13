2df2d71f8398cb55c3584452d3ad1337598c9a8e
Ibai Clase 1 <ibai3553@gmail.com>
2021-04-13 12:58:56 +0300

    owo

--- 01 Calculadora/01 Calculadora/Form1.vb
+++ 01 Calculadora/01 Calculadora/Form1.vb
@@ -27,34 +27,37 @@
 
             Return
         End If
+
+
+
         Dim num1 As Integer = txtNum1.Text
         Dim num2 As Integer = txtNum2.Text
         Dim res As Double = 0
 
-        If rbSuma.Checked Then
-            res = num1 + num2
 
-            txtRes.Text() = res
-        End If
-        If rbResta.Checked Then
-            res = num1 - num2
+        Select Case True
+            Case rbSuma.Checked
+                res = num1 + num2
 
-            txtRes.Text() = res
-        End If
-        If rbMulti.Checked Then
-            res = num1 * num2
+                txtRes.Text() = res
+            Case rbResta.Checked
+                res = num1 - num2
 
-            txtRes.Text() = res
-        End If
-        If rbDiv.Checked Then
-            If num2 = 0 Then
-                MessageBox.Show("Eres Debil, tus trucos no funcionan", "Dios no existe")
-                Return
-            End If
-            res = num1 / num2
+                txtRes.Text() = res
+            Case rbMulti.Checked
+                res = num1 * num2
 
-            txtRes.Text() = res
-        End If
+                txtRes.Text() = res
+            Case rbDiv.Checked
+                If num2 = 0 Then
+                    MessageBox.Show("Eres Debil, tus trucos no funcionan", "Dios no existe")
+                    Return
+                End If
+                res = num1 / num2
+
+                txtRes.Text() = res
+        End Select
+
 
     End Sub
 
@@ -68,9 +71,17 @@
     End Sub
 
     Private Sub Button1_Click_2(sender As Object, e As EventArgs) Handles btnSalir.Click
-        MessageBox.Show("No puedes escapar para siempre", "Dios no existe")
 
-        Close()
+        If MessageBox.Show("No puedes escapar para siempre", "Dios no existe", MessageBoxButtons.YesNo) = DialogResult.Yes Then
+
+            Close()
+
+        End If
+
+
+    End Sub
+
+    Private Sub txtNum1_TextChanged(sender As Object, e As EventArgs) Handles txtNum1.TextChanged
 
     End Sub
 End Class
